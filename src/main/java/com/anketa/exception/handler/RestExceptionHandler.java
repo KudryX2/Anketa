5c20accb2e1a65d495682d734599f06d6b7cc56d
Kudry <15013415+KudryX2@users.noreply.github.com>
2023-11-24 13:49:54 +0100

    Unexpected exception handling and logging
    - Prevents client from getting internal server errors

--- src/main/java/com/anketa/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/anketa/exception/handler/RestExceptionHandler.java
@@ -1,18 +1,31 @@
 package com.anketa.exception.handler;
 
 import com.anketa.exception.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class RestExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+        logger.info("BadRequestException " + ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception ex) {
+        logger.warn("Unexpected exception " + ex.getMessage());
+        return new ResponseEntity<>("Something went wrong try again later", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
